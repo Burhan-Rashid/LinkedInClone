b3159deb0db88ba85ebd781a8f06682344e59bb4
Burhan Rashid <34839657+Burhan-Rashid@users.noreply.github.com>
2021-08-16 11:39:28 +0530

    Added the Login/Register pages with functionality(without styling)

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 import Widgets from "./components/Widgets";
+import { selectUser } from './state/userSlice';
+import Register from "./components/Register";
+import { useSelector } from 'react-redux';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="App">
       <Header />
-      <div className="content">
-        <Sidebar />
-        <Main />
-        <Widgets />
-      </div>
+      {!user ? <Register /> :
+        <div className="content">
+          <Sidebar />
+          <Main />
+          <Widgets />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/components/Register.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import "./Register.css"
+import { auth } from "../Firebase"
+import { login } from '../state/userSlice';
+import { useDispatch } from 'react-redux';
+
+function Register() {
+    const dispatch = useDispatch();
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [name, setName] = React.useState("");
+    const [photo, setPhoto] = React.useState("");
+    const [registerType, setRegisterType] = React.useState(false);
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        try {
+            auth.createUserWithEmailAndPassword(email, password).then((res) => {
+                res.user.updateProfile({
+                    displayName: name,
+                    photoURL: photo
+                }).then(
+                    dispatch(login({
+                        email: res.user.email,
+                        uid: res.user.uid,
+                        displayName: name,
+                        photoUrl: photo
+                    }))
+                )
+            })
+
+        } catch (error) { alert(error) }
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        auth.signInWithEmailAndPassword(email, password)
+            .then((res) => {
+                dispatch(login({
+                    email: res.user.email,
+                    uid: res.user.uid,
+                    displayName: res.user.displayName,
+                    photoUrl: res.user.photoURL
+                }))
+            })
+            .catch(error => alert(error.message))
+    }
+
+    const handleType = () => {
+        setRegisterType(!registerType);
+    }
+
+    return (
+        <div className="register">
+            {registerType ?
+                <div>
+                    <h3>Register</h3>
+                    <form>
+                        <input type="text" value={name} placeholder="Full Name" onChange={(e) => setName(e.target.value)} />
+                        <input type="text" value={photo} placeholder="Photo Url (optional)" onChange={(e) => setPhoto(e.target.value)} />
+                        <input type="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                        <button type="submit" onClick={handleRegister} >Register</button>
+                    </form>
+                    <div>
+                        <h6>Already have an account?</h6><h5 onClick={handleType}>Login here</h5>
+                    </div>
+                </div>
+                :
+                <div className="login">
+                    <h3>Login</h3>
+                    <form>
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <button type="submit" onClick={handleLogin} > Sign In </button>
+                    </form>
+                    <div>
+                        <h6>Don't have an account?</h6><h5 onClick={handleType}>Register here</h5>
+                    </div>
+                </div>}
+        </div>
+    )
+}
+
+export default Register
