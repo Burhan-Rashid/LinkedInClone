2baca72d01815c41cb7c600effc8b4106bc7a6c5
Burhan Rashid <34839657+Burhan-Rashid@users.noreply.github.com>
2021-08-17 11:30:17 +0530

    Added the logout functionality and some login/register/header styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 import Widgets from "./components/Widgets";
-import { selectUser } from './state/userSlice';
+import { selectUser } from "./state/userSlice";
 import Register from "./components/Register";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 function App() {
   const user = useSelector(selectUser);
 
   return (
-    <div className="App">
-      <Header />
-      {!user ? <Register /> :
-        <div className="content">
-          <Sidebar />
-          <Main />
-          <Widgets />
+    <>
+      {!user ? (
+        <Register />
+      ) : (
+        <div className="App">
+          <Header />
+          <div className="content">
+            <Sidebar />
+            <Main />
+            <Widgets />
+          </div>
         </div>
-      }
-    </div>
+      )}
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,37 @@
-import React from 'react';
-import './Header.css';
-import HeaderOption from './HeaderOption';
+import React from "react";
+import "./Header.css";
+import HeaderOption from "./HeaderOption";
 import { FaSistrix, FaHome } from "react-icons/fa";
 import { MdGroup, MdMessage } from "react-icons/md";
 import { IoBagRemoveSharp, IoNotifications } from "react-icons/io5";
 
 function Header() {
-    return (
-        <div className="header">
-            <div className="header__left">
-                <img className="header__icon" src="/assets/linkedin.png" alt="...icon" />
-                <div className="header__searchDiv">
-                    <FaSistrix className="header__searchIcon" />
-                    <input className="header__searchInput" type="text" />
-                </div>
-            </div>
-            <div className="header__right">
-                <HeaderOption icon={<FaHome size={24} />} title="Home" />
-                <HeaderOption icon={<MdGroup size={24} />} title="Network" />
-                <HeaderOption icon={<IoBagRemoveSharp size={24} />} title="Jobs" />
-                <HeaderOption icon={<MdMessage size={24} />} title="Messaging" />
-                <HeaderOption icon={<IoNotifications size={24} />} title="Notifications" />
-                {/* <HeaderOption avatar={true} title="Me" /> */}
-            </div>
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          className="header__icon"
+          src="/assets/linkedin.png"
+          alt="...icon"
+        />
+        <div className="header__searchDiv">
+          <FaSistrix className="header__searchIcon" />
+          <input className="header__searchInput" type="text" />
         </div>
-    )
+      </div>
+      <div className="header__right">
+        <HeaderOption icon={<FaHome size={24} />} title="Home" />
+        <HeaderOption icon={<MdGroup size={24} />} title="Network" />
+        <HeaderOption icon={<IoBagRemoveSharp size={24} />} title="Jobs" />
+        <HeaderOption icon={<MdMessage size={24} />} title="Messaging" />
+        <HeaderOption
+          icon={<IoNotifications size={24} />}
+          title="Notifications"
+        />
+        <HeaderOption avatar={true} title="Me" />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
